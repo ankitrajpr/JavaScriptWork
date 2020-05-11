4e4e02e928e7190b3cdcd1c076a25416df3d2915
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2020-05-11 21:32:48 +0530

    IF Block checkin

---
+++ IF.js
@@ -0,0 +1,17 @@
+/* 
+//Flow COntrol
+let temp =32
+let isFreezing = temp < 32
+
+
+//Check if isFreezing is true , if ys then go inside of if block statement
+if (isFreezing) {
+console.log("It is hot outside")
+} */
+
+let temp = 32
+
+if (temp <= 32) {
+    console.log('It is hot outside')
+}
+
