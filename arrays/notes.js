bd75aae167bb43099a4f9a329704d9f391e8a108
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2020-06-06 20:35:38 +0530

    Arrays Basics

---
+++ arrays/notes.js
@@ -0,0 +1,7 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(notes.length)
+
+notes[0] = 'Note 12'
+console.log(notes[0])
+console.log(notes[notes.length - 3])
---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['Order cat food', 'Clean Kitchen', 'Buy Food', 'Do Work', ' Exercise Files']
+
+console.log(`You have ${todos.length} todos!`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todos: ${todos[todos.length - 2]}`)
