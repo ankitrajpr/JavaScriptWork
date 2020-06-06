94b135dc3ff9ca655cc0a8778c045ec23ac9f3f4
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2020-06-06 21:37:48 +0530

    Manipulate array with pre-methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,7 +1,24 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+/* const notes = ['Note 1', 'Note 2', 'Note 3']
 
 console.log(notes.length)
 
 notes[0] = 'Note 12'
 console.log(notes[0])
-console.log(notes[notes.length - 3])
+console.log(notes[notes.length - 3]) */
+
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+/* console.log(notes.pop())  //remove the last element
+notes.push('Note 4')
+
+console.log(notes.shift())
+notes.unshift('My first Note') */
+
+//notes.splice(1,1)
+//notes.splice(1,0,'This is the new 1st index but second item')
+//notes.splice(1, 1, 'This is the new 1st index but second item')
+
+
+
+console.log(notes.length)
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,18 @@
-const todos = ['Order cat food', 'Clean Kitchen', 'Buy Food', 'Do Work', ' Exercise Files']
+/* const todos = ['Order cat food', 'Clean Kitchen', 'Buy Food', 'Do Work', ' Exercise Files']
 
 console.log(`You have ${todos.length} todos!`)
 console.log(`Todo: ${todos[0]}`)
-console.log(`Todos: ${todos[todos.length - 2]}`)
+console.log(`Todos: ${todos[todos.length - 2]}`) */
+
+const todos = ['Order cat food', 'Clean Kitchen', 'Buy Food', 'Do Work', ' Exercise Files']
+
+todos.splice(2,1)
+console.log(todos)
+todos.push('New Iteam at ENd')
+
+console.log(todos)
+
+todos.shift()
+console.log(todos)
+
+console.log(`You have ${todos.length} todos!`)
