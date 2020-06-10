477cc7d288d7f26b1b2976d695b4453ba8ec780c
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2020-06-10 23:56:07 +0530

    Query Selector

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,16 @@
-console.log('New File loaded for a new JS')
+//console.log('New File loaded for a new JS')
+//DOM Documnet Object Model Here, Object in DOM is a JS Object
+
+// Documnet is modeled using a JS object and that obj is called documnet
+//Query and Remove
+/* const p = document.querySelector('p')
+//console.log(p)
+p.remove() */
+
+//Query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p){
+    p.textContent = '*********'
+    //console.log(p.textContent)
+    //p.remove()
+})
